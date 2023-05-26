6c177112979fba17e355b5c59608b2f4f699b0c9
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-26 20:49:42 +0200

    Completes task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach(galleryItem => {
+  gallery.insertAdjacentHTML("beforeend", `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          data-source="${galleryItem.original}"
+          alt="${galleryItem.description}"
+        />
+      </a>
+    </li>
+  `);
+});
+
+gallery.addEventListener("click", event => {
+  event.preventDefault();
+  if (event.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(`
+      <img
+        src="${event.target.getAttribute("data-source")}"
+        alt="${event.target.getAttribute("alt")}
+        width="800"
+        height="600">
+    `);
+
+    instance.show();
+
+    document.addEventListener("keydown", event => {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    });
+  }
+});
