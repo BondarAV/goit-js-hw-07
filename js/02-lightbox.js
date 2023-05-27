2ade80a1584025def5aef80da0974624f443ad28
Andrii Bondar <77549085+BondarAV@users.noreply.github.com>
2023-05-27 15:25:44 +0200

    Completes task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach(galleryItem => {
+  gallery.insertAdjacentHTML("beforeend", `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          alt="${galleryItem.description}"
+        />
+      </a>
+    </li>
+  `);
+});
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
